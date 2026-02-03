2a69aee5242377704d352ae99f9754ae9c873ab7
Oliver García <oliver.garcia.aguado@gmail.com>
2026-02-03 16:34:09 +0100

    YA FUNCIONA (POR UNA LETRA FALLABA)
    omg

--- IG1App/Cube.cpp
+++ IG1App/Cube.cpp
@@ -22,14 +22,39 @@ void Cube::render(const glm::mat4& modelViewMat) const
 
 		glCullFace(GL_BACK);  // Oculta la cara frontal para que los procesos que ocurran ahora solo afecten a la trasera
 
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINES);  // LINEAS
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);  // LINEAS
 		mMesh->render();
 
 
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		/*glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);*/
 		glDisable(GL_CULL_FACE);
 
 	}
+
+	//if (mMesh == nullptr) return;
+
+	//glm::mat4 aMat = modelViewMat * mModelMat;
+	//mShader->use();
+	//upload(aMat);
+
+	////Para cara BACK con relleno
+	//glEnable(GL_CULL_FACE);
+	//glCullFace(GL_FRONT);  // Descarta la cara FRONT
+
+	//// Dibujar con punto
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+	//mMesh->render();
+
+	//// Para cara FRONT con líneas
+	//glCullFace(GL_BACK);   // Descarta la cara BACK
+
+	//// Dibujar con líneas
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//mMesh->render();
+
+	////Restaurar estado de OpenGL
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	//glDisable(GL_CULL_FACE);
 
 
 }
