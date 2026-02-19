9fe3045f4957658cf0488b039755b735a9754a1f
He Deng <he.deng1230@gmail.com>
2026-02-19 12:01:48 +0100

    fking lio con git

--- IG1App/Entity/Ground.h
+++ IG1App/Entity/Ground.h
@@ -1,8 +1,8 @@
 #pragma once
-//#include "EntityWithTexture.h"
-#include "Entity.h"
+#include "EntityWithTexture.h"
+//#include "Entity.h"
 
-class Ground : public EntityWithColors
+class Ground : public EntityWithTexture
 {
 public:
     Ground(GLdouble length, const char* shader);
